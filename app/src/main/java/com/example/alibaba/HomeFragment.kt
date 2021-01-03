36faaad7e1dfe89d97e966eb8cbb40c70313042c
alivssut <59310924+alivssut@users.noreply.github.com>
2021-01-03 20:40:28 +0330

    first

--- app/src/main/java/com/example/alibaba/HomeFragment.kt
+++ app/src/main/java/com/example/alibaba/HomeFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class HomeFragment : Fragment(){
 
@@ -15,6 +17,14 @@ class HomeFragment : Fragment(){
     ): View? {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
+
+        var  registerFragment = RegisterFragment()
+
+        val fragmentManager: FragmentManager = getFragmentManager()!!
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.home_register_container, registerFragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
 
 
         return root
